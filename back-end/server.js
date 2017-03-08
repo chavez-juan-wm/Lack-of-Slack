834035100ff358b16c7d67bc7f1824f4e367813a
Juan Chavez <juan.chavez@west-mec.org>
2017-03-08 16:13:19 -0700

    Post requests functionality to add information to the database, and converting it to work with mongoose

--- back-end/server.js
+++ back-end/server.js
@@ -1,14 +1,53 @@
+/*
+    Author: Juan M. Chavez
+    Date: 3-3-17
+    File: server.js
+    Description: Back-end server created with Express
+
+    3/3
+        Setup a basic server on port 8080 with Express
+        Added an endpoint for message posts
+        Included body-parser to parse the body that comes with the request
+
+    3/6
+        Added middleware to fix: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+        Added mongodb support using mongoose
+ */
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var port = 8080;
+
+// Database, Schema, and Model
+var database;
+//var Message = mongoose.model({
+//    message: String
+//});
 
 app.use(bodyParser.json());
 
+// Middleware to fix a CORS issue
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
+// Endpoint for post requests to '/api/message'
 app.post('/api/message', function(req, res){
     console.log(req.body);
-    res.send('Post successful!');
+    database.collection('messages').insertOne(req.body);
     res.status(200);
+});
+
+// Connection to mongodb database
+mongoose.connect('mongodb://localhost:27017/test', function(err, db){
+    if(!err){
+        console.log('Connected to MongoDB');
+        database = db;
+    }
 });
 
 var server = app.listen(port, function(){
--- front-end/src/app/main/main.controller.js
+++ front-end/src/app/main/main.controller.js
@@ -4,22 +4,30 @@
     File: main.controller.js
     Description: This is the controller used in main.html
 
-    3/2 JMC
+    3/2
         Removed the sample content
         Added the $log service for testing
         Created the postMessage() method
+
+    3/3
+        Replaced the $log service with $http
+        Added a post request in the postMessage() method with a message for debugging
+
+    3/6
+        Changed the data sent in the post request to what is in the textarea in main.html
 */
 
 export class MainController {
-    constructor ($log) {
+    constructor ($http) {
         'ngInject';
 
         // Assign properties for this class
+        this.$http = $http;
         this.$log = $log;
     }
 
-    // Method to handle $http post requests for creating messages
+    // Method to handle initiating $http post requests for creating messages
     postMessage() {
-        this.$log.log("Post");
+        this.$http.post('http://localhost:8080/api/message', { message: this.message });
     }
 }
