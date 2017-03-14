99bb3b84cf406fe509457a17df2487522b4117ab
Juan Chavez <juan.chavez@west-mec.org>
2017-03-14 15:58:32 -0700

    Set up the back end to process get requests for the messages in the database and set up the front end to display the messages.

--- back-end/server.js
+++ back-end/server.js
@@ -12,6 +12,11 @@
     3/6
         Added middleware to fix: No 'Access-Control-Allow-Origin' header is present on the requested resource.
         Added mongodb support using mongoose
+        Inserted req.body into a database in the post for 'api/message'
+
+    3/8
+        Changed the mongodb insert to a mongoose insert
+        Created an endpoint for get requests for 'api/message'
  */
 
 var express = require('express');
@@ -22,9 +27,11 @@ var port = 8080;
 
 // Database, Schema, and Model
 var database;
-//var Message = mongoose.model({
-//    message: String
-//});
+
+// Mongoose's method to create a model/class
+var Message = mongoose.model('Message', {
+    msg: String
+});
 
 app.use(bodyParser.json());
 
@@ -35,12 +42,25 @@ app.use(function(req, res, next){
     next();
 });
 
+// Endpoint for the client to retrieve all of the messages in the database
+app.get('/api/message', getMessages);
+
 // Endpoint for post requests to '/api/message'
 app.post('/api/message', function(req, res){
     console.log(req.body);
-    database.collection('messages').insertOne(req.body);
+
+    // Create a Message object and insert a string/req.body, then save it
+    var message = new Message(req.body);
+    message.save();
     res.status(200);
 });
+
+// Get the messages from the database
+function getMessages(req, res){
+    Message.find({}).exec(function(err, data){
+        res.send(data);
+    });
+}
 
 // Connection to mongodb database
 mongoose.connect('mongodb://localhost:27017/test', function(err, db){
--- front-end/src/app/main/main.controller.js
+++ front-end/src/app/main/main.controller.js
@@ -15,19 +15,37 @@
 
     3/6
         Changed the data sent in the post request to what is in the textarea in main.html
+
+    3/9
+        Added a get messages method to retrieve the messages from the database and set them to a property
 */
 
 export class MainController {
-    constructor ($http) {
+    constructor ($http, $log) {
         'ngInject';
 
         // Assign properties for this class
         this.$http = $http;
         this.$log = $log;
+        this.getMessages();
     }
 
     // Method to handle initiating $http post requests for creating messages
     postMessage() {
-        this.$http.post('http://localhost:8080/api/message', { message: this.message });
+        this.$http.post('http://localhost:8080/api/message', { msg: this.message });
+    }
+
+    getMessages(){
+        // View model that is a reference to itself
+        var vm = this;
+        this.$http.get('http://localhost:8080/api/message')
+        .then(
+            function(result){
+                vm.messages = result.data;
+            },
+            function(error){
+                vm.$log.error(error);
+            }
+        );
     }
 }
